d87e0623b2f38c20be1ad28b6ce6600772304b70
Your Name <dianazenteno10@gmail.com>
2018-06-13 11:57:29 -0400

    Primer commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,31 @@
+import { RouterModule, Routes } from '@angular/router';
+
+import { PagesComponent } from './pages/pages.component';
+
+
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './login/register.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+
+
+
+
+
+const appRoutes: Routes = [
+    //    { path: '',
+    //      component: PagesComponent,
+    //      children: [
+    //      { path: 'dashboard', component: DashboardComponent},
+    //      { path: 'progress', component: ProgressComponent },
+    //      { path: 'graficas1', component: Graficas1Component },
+    //      { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
+    //      ]
+    //    },
+
+        { path: 'login', component: LoginComponent },
+        { path: 'register', component: RegisterComponent },
+        { path: '**', component: NopagefoundComponent }
+
+];
+
+export const APP_ROUTER = RouterModule.forRoot ( appRoutes, { useHash: true } );
---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { PAGES_ROUTES } from './pages.routes';
+
+//////// ModuloShared
+import { SharedModule } from '../shared/shared.module';
+
+
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+import { ProgressComponent } from './progress/progress.component';
+import { PagesComponent } from './pages.component';
+
+
+
+
+@NgModule ({
+    declarations: [
+        PagesComponent,
+        DashboardComponent,
+        Graficas1Component,
+        ProgressComponent
+    ],
+    exports: [
+        DashboardComponent,
+        Graficas1Component,
+        ProgressComponent
+    ],
+    imports: [
+        SharedModule,
+        PAGES_ROUTES
+    ]
+})
+export class PagesModule  { }
